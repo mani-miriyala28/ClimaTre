2e6e5cb0fc8148c010581b9940e18d0c9d76e21f
mani-miriyala28 <111078616+mani-miriyala28@users.noreply.github.com>
2025-01-07 00:40:53 +0530

    Configured App Routes and Layout

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,42 @@
+import { useTheme } from "@/context/theme-provider";
+import { Moon, Sun } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark";
+  return (
+    <header className="sticky top-0 z-50 w-full border-b  bg-background/95 backdrop-blur py-2  supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto flex h-16 justify-between items-center px-4">
+        <Link to={"/"}>
+          <img
+            src={theme === "dark" ? "./dark.png" : "./light.png"}
+            alt="Climate Logo"
+            className={`h-14 ${isDark ? 'bg-[#0a0a0a]' : "bg-[#ffffff]"}`}
+          />
+        </Link>
+        <div>
+          {/*Search Bar*/}
+          {/*toggle button*/}
+          <div
+            className={`flex items-center cursor-pointer transition-transform duration-500  ${
+              isDark ? "rotate-180" : "rotate-0"
+            }`}
+            onClick={() => {
+              setTheme(isDark ? "light" : "dark");
+            }}
+          >
+            {isDark ? (
+                <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" />
+            ) : (
+                <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all" />
+                )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
