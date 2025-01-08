8db61ff6002361bdd39b8e281f3723b0f291023c
mani-miriyala28 <111078616+mani-miriyala28@users.noreply.github.com>
2025-01-08 16:47:24 +0530

    ClimaTre

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 import Header from "./Header";
 
 const Layout = ({ children }: PropsWithChildren) => {
